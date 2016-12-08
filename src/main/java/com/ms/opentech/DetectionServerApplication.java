95dcb7c0ce4c8166ce5e4b29e4283972ede473e9
JieWang <t-jiewa@microsoft.com>
2016-12-08 10:56:26 +0800

    init commit

---
+++ src/main/java/com/ms/opentech/DetectionServerApplication.java
@@ -0,0 +1,12 @@
+package com.ms.opentech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectionServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ms/opentech/DetectionServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ms.opentech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DetectionServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
